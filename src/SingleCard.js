c0a701f821052c9e8004556de16ab34f58d4f58f
meavehb <60469453+meavehb@users.noreply.github.com>
2022-03-27 22:56:28 +0800

    added cover img code

--- src/SingleCard.js
+++ src/SingleCard.js
@@ -6,7 +6,7 @@ export default function SingleCard({ card }) {
       <div className="card">
         <div>
           <img className="front" src={card.src} alt="card front" />
-          <img className="back" src={img / cover.png} alt="card back" />
+          <img className="back" src="/img/cover.png" alt="card back" />
         </div>
       </div>
     </div>
