3fcf58a0bc7209e3e32afbc0b9b9055889423563
meavehb <60469453+meavehb@users.noreply.github.com>
2022-03-27 21:21:04 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Card Matching</h1>
+      <button>Start!</button>
+    </div>
+  );
+}
+
+export default App;
